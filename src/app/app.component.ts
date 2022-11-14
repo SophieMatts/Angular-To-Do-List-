fa19bb18086d12974279d7f86baaa4b67585cbe9
Sophie Mattacks <sophie.mattacks@ad.infosys.com>
2022-11-14 11:27:29 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [
+    new Task("Visit Ann"),
+    new Task("Call Dad"),
+    new Task("Go to the gym"),
+    new Task("Wash the dishes"),
+    new Task("Shop for the party")
+  ]
+
+  add(newTask: string){
+    this.tasks.push(new Task(newTask));
+  }
+
+  remove(existingTask: Task){
+    var userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${existingTask}" `)
+
+    if(userConfirmed){
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
+
+  markAsDone(task: Task){
+    task.isDone = true;
+  }
+
+}
+
+class Task {
+  constructor(public title: string){
+
+  }
+
+  public isDone = false; 
 }
