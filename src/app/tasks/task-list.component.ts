0e8996cefec4be84049b65b9f0938b30a53894d1
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-11-17 13:16:10 +0000

    Implemented to Task Service: get, add and remove

--- src/app/tasks/task-list.component.ts
+++ src/app/tasks/task-list.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Route } from '@angular/router';
 import { NewTask } from './new-task.dto';
 import { TaskItem } from './task-item.dto';
+import { TaskService } from './task.service';
 
 @Component({
   selector: 'task-list',
@@ -13,31 +14,23 @@ export class TaskListComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) { }
 
+  taskService = new TaskService();
+  tasks = this.taskService.getAllTasks();
 
   newTask: NewTask = new NewTask();
-
 
   ngOnInit(): void {
     var strDate = this.route.snapshot.params['date'];
     this.newTask = new NewTask(this.newTask.title, new Date(strDate));
   }
 
-
-  tasks: TaskItem[] = [
-    new TaskItem("Visit Ann"),
-    new TaskItem("Call Dad"),
-    new TaskItem("Go to the gym"),
-    new TaskItem("Wash the dishes"),
-    new TaskItem("Shop for the party")
-  ]
-
   add(taskNgForm: NgForm){
 
     if(taskNgForm.touched == false){
       return;
     }
+    this.taskService.addTask(this.newTask);
 
-    this.tasks.push(new TaskItem(this.newTask.title))
     taskNgForm.reset({date: this.newTask.date});
   }
 
@@ -45,7 +38,7 @@ export class TaskListComponent implements OnInit {
     var userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${existingTask.title}" `)
 
     if(userConfirmed){
-      this.tasks = this.tasks.filter(task => task != existingTask);
+      this.taskService.removeTask(existingTask);
     }
   }
 
