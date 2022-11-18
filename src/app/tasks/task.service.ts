79e8b29245833638e0a21883dba0bc702dcdfb05
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-11-18 12:21:03 +0000

    Added GET request for getAllTasks and POST request for addTask

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NewTask } from './new-task.dto';
 import { TaskItem } from './task-item.dto';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, tap, map } from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable()
@@ -12,12 +12,24 @@ export class TaskService {
   private tasks = new BehaviorSubject<TaskItem[]>([])
 
   getAllTasks(): Observable<TaskItem[]>{
-   return this.httpClient.get<TaskItem[]>('http://localhost:3001/tasks')
+   this.httpClient.get<TaskItem[]>('http://localhost:3001/tasks')
+   .pipe(tap(console.log))
+   .pipe(map(TaskService.mapTaskItems))
+   .pipe(tap(console.log))
+   .subscribe(t => this.tasks.next(t))
+
+   return this.tasks
+  }
+
+  private static mapTaskItems(items: {title: string}[]){
+    return items.map(item => new TaskItem(item.title))
   }
 
   addTask(newTask: NewTask){
     var updatedTasks = this.tasks.value.concat(new TaskItem(newTask.title));
-    this.tasks.next(updatedTasks);
+
+    this.httpClient.post('http://localhost:3001/tasks', newTask)
+    .subscribe(() => this.tasks.next(updatedTasks))
   }
 
   removeTask(existingTask: TaskItem){
